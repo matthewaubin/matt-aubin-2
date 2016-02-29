871d73f618bbe6fd062f61a0fdefe5a1bcbdc955
Nick Buonincontri <nbuonin@gmail.com>
2016-02-29 01:12:20 -0500

    remove indicators for scrollmagic

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
   var scene = new ScrollMagic.Scene({triggerElement: ".home-page", duration: 200})
     // animate color and top border in relation to scroll position
     .setTween("nav", {backgroundColor: "#ffffff", boxShadow: "0 2px 8px 0 rgba(0,0,0,0.5),0 -5px 3px -10px #fff", color: "#000000"}) // the tween durtion can be omitted and defaults to 1
-    .addIndicators({name: "1 (duration: 450)"}) // add indicators (requires plugin)
+    //.addIndicators({name: "1 (duration: 450)"}) // add indicators (requires plugin)
     .addTo(controller);
 
 
